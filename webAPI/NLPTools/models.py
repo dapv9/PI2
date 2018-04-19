ec526f725f5722ed1f24b962404c8a8a30f65352
david parra <dapv9@hotmail.com>
2018-04-19 06:02:06 -0500

    formulario, edicion y listado

--- webAPI/NLPTools/models.py
+++ webAPI/NLPTools/models.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.shortcuts import render
 from django.db import models
-#from django.utils import timezone
+import nltk
+
+nltk.download('book')
+
 
 # Create your models here.
 
 
 class Post(models.Model):
-	writter = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-        text = models.CharField(max_length=300)
+    writter = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    token = models.CharField(max_length=500)
 
-        def frase(txt):
+    def texto(txt):
             txt.save()
 
-        def __str__(txt):
-            return txt.text
+    def __str__(txt):
+            return txt.token
+    
+    def tokenizer(txt):
+	        txt.token = nltk.Text(nltk.word_tokenize(txt.text))
+	        return txt
+
+    def tagging(tokenizelist):
+	        tag = nltk.pos_tag(tokenizelist)
+	        return tag
--- webAPI/NLPTools/views.py
+++ webAPI/NLPTools/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
 from .forms import PostForm
+import nltk
 
 # Create your views here.
 
@@ -17,11 +18,14 @@ def post_detail(request, pk):
     	return render(request, 'NLPTools/post_detail.html', {'post': post})
 
 def post_new(request):
+	
 	if request.method == "POST":
 		form=PostForm(request.POST)
 		if form.is_valid():
                 	post = form.save(commit=False)
                 	post.writter = request.user
+					
+
                 	post.save()
                 	return redirect('post_detail', pk=post.pk)
 	else:
@@ -35,6 +39,7 @@ def post_edit(request, pk):
             if form.is_valid():
                 post = form.save(commit=False)
                 post.writter = request.user
+				
                 post.save()
                 return redirect('post_detail', pk=post.pk)
         else:

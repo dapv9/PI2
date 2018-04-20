10fc6fcd7cd542942ce75e551832fe68568697c4
david parra <dapv9@hotmail.com>
2018-04-19 22:31:27 -0500

    formulario simple tokeniza

--- webAPI/NLPTools/models.py
+++ webAPI/NLPTools/models.py
@@ -2,9 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.db import models
-import nltk
 
-nltk.download('book')
 
 
 # Create your models here.
@@ -15,16 +13,17 @@ class Post(models.Model):
     text = models.CharField(max_length=300)
     token = models.CharField(max_length=500)
 
+
     def texto(txt):
-            txt.save()
+                txt.save()
 
     def __str__(txt):
-            return txt.token
+                return txt.token
     
-    def tokenizer(txt):
-	        txt.token = nltk.Text(nltk.word_tokenize(txt.text))
-	        return txt
+#    def tokenizer(txt):
+# 	        txt.token = nltk.Text(nltk.word_tokenize(txt.text))
+# 	        return txt.token
 
-    def tagging(tokenizelist):
-	        tag = nltk.pos_tag(tokenizelist)
-	        return tag
+#     def tagging(tokenizelist):
+# 	        tag = nltk.pos_tag(tokenizelist)
+# 	        return tag
--- webAPI/NLPTools/views.py
+++ webAPI/NLPTools/views.py
@@ -5,16 +5,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
 from .forms import PostForm
-import nltk
+from nltk import word_tokenize, pos_tag, ne_chunk
 
 # Create your views here.
 
 def post_list(request):
 	posts = Post.objects.all().order_by('id')
+
    	return render(request, 'NLPTools/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
+
     	return render(request, 'NLPTools/post_detail.html', {'post': post})
 
 def post_new(request):
@@ -22,26 +24,27 @@ def post_new(request):
 	if request.method == "POST":
 		form=PostForm(request.POST)
 		if form.is_valid():
-                	post = form.save(commit=False)
-                	post.writter = request.user
-					
-
-                	post.save()
-                	return redirect('post_detail', pk=post.pk)
+			post = form.save(commit=False)
+			post.writter = request.user
+			post.token = ne_chunk(pos_tag(word_tokenize(post.text)))
+			post.save()
+			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
-	return render(request,'NLPTools/post_edit.html',{'form':form})
+		return render(request,'NLPTools/post_edit.html',{'form':form})
 
 def post_edit(request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        if request.method == "POST":
-            form = PostForm(request.POST, instance=post)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.writter = request.user
-				
-                post.save()
-                return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm(instance=post)
-        return render(request, 'NLPTools/post_edit.html', {'form': form})
+    			post = get_object_or_404(Post, pk=pk)
+
+
+			if request.method == "POST":
+				form = PostForm(request.POST, instance=post)
+				if form.is_valid():
+					post = form.save(commit=False)
+					post.writter = request.user
+					post.token=ne_chunk(pos_tag(word_tokenize(post.text)))
+					post.save()
+					return redirect('post_detail', pk=post.pk)
+			else:
+				form = PostForm(instance=post)
+        		return render(request, 'NLPTools/post_edit.html', {'form': form})
